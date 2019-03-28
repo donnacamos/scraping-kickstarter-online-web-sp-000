5b5ce291352eee8c9786dd725a9b9a3adeb3d9ee
Donna Amos <donnacamos.developer@gmail.com>
2019-03-28 23:57:39 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,7 +14,7 @@ kickstarter = Nokogiri::HTML(html)
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
-
+g
 create_project_hash 
 binding.pry 
 end 
