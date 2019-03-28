d8054cc3ed1d226dfa3ba0f1d651c127606103d6
Donna Amos <donnacamos.developer@gmail.com>
2019-03-28 23:51:04 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,7 +2,7 @@
 require 'nokogiri'
 require 'pry'
 
-require_relative '/'
+require_relative './fixtures/kickstarter.html' 
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
