6668d3500dd6c4b36e3a6cc58a3a0dcfab0e3f81
Donna Amos <donnacamos.developer@gmail.com>
2019-03-28 23:49:55 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -5,11 +5,11 @@ require 'pry'
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
  
-kickstarter = Nokogiri::HTML(html)
-  binding.pry 
+kickstarter = Nokogiri::HTML(html) 
 
   
 # projects: kickstarter.css("li.project.grid_4")  
     
 create_project_hash 
+binding.pry 
 end 
