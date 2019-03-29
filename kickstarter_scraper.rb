15c05eff927dfefd95d66e846a4ebc0172039623
Donna Amos <donnacamos.developer@gmail.com>
2019-03-29 00:10:27 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,13 @@
 require 'nokogiri'
 require 'pry'
 
+# projects: kickstarter.css("li.project.grid_4")  
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+
 require_relative './fixtures/kickstarter.html' 
 
 def create_project_hash
@@ -10,12 +17,6 @@ def create_project_hash
 kickstarter = Nokogiri::HTML(html) 
 
   
-# projects: kickstarter.css("li.project.grid_4")  
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
-# description: project.css("p.bbcard_blurb").text
-# location: project.css("ul.project-meta span.location-name").text
-# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 create_project_hash 
 binding.pry 
 end 
